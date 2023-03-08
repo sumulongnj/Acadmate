4a9fcce5798c1e8565f93858faea7ceaf1fa9d7d
shannentan22 <astan9@up.edu.ph>
2023-03-08 11:09:58 +0800

    Modified dashboard

--- acadmate/src/App.svelte
+++ acadmate/src/App.svelte
@@ -1,20 +1,22 @@
 <script>
-	export let name;
+	var header = document.getElementById("myDIV");
+	var btns = document.getElementsByClassName("btn");
+	
 </script>
 
 <main>
 	<div class="topnav">
 		<!-- Substitute links below with image elements. -->
-		<a href="Acadmate">Acadmate</a>
+		<a href="/">Acadmate</a>
 		<a href="Settings" style="float: right">Settings</a>
 		<a href="Profile" style="float: right">Profile</a>
 	</div>
 	<div class="sidenav">
 		<ul>
-			<li><a href="Dashboard">Dashboard</a></li>
-			<li><a href="My-Curriculum">My Curriculum</a></li>
-			<li><a href="Grades">Grade Alerts</a></li>
-			<li><a href="Requirements">Requirements</a></li>
+			<li><a href="/" class="btn active">Dashboard</a></li>
+			<li><a href="/" class="btn inactive">My Curriculum</a></li>
+			<li><a href="/" class="btn inactive">Grade Alerts</a></li>
+			<li><a href="/" class="btn inactive">Requirements</a></li>
 		</ul>
 	</div>
 </main>
@@ -31,6 +33,7 @@
 		overflow: hidden;
 		position: fixed;
 		z-index: 1;
+		box-shadow: 0px 5px 10px lightgray;
 	}
 	.topnav a {
 		float: left;
@@ -53,20 +56,34 @@
 		position: fixed;
 		font-weight: 600;
 	}
+	
 	.sidenav ul {
 		list-style-type: none;
 		margin: 0;
+		margin-left: 25px;
 		padding: 0;
-		width: 200px;
+		width: 150px;
 	}
 	.sidenav li a {
 		display: block;
-		color: #898989;
 		padding: 8px 16px;
 		text-decoration: none;
+		margin-top: 10px;
+		margin-bottom: 10px;
+		border-radius: 10px;
+		/* color: #898989; */
 	}
-	.sidenav li a:hover {
+	.btn:hover {
 		background-color: #ac4949;
 		color: white;
+	}
+	.active {
+		background-color: #ac4949;
+		color: white;
+	}
+
+	.inactive {
+		background-color: #ffffff;
+		color: #898989;
 	}
 </style>
