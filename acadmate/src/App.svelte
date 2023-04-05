1bbf6479d1490c0fb349378968d1aefde3f29113
shannentan22 <astan9@up.edu.ph>
2023-04-05 10:40:51 +0800

    Working router

--- acadmate/src/App.svelte
+++ acadmate/src/App.svelte
@@ -5,31 +5,25 @@
 	import Grades from './pages/Grades.svelte'; 
 	import GradeAlerts from './pages/GradeAlerts.svelte';
 	import Requirements from './pages/Requirements.svelte'; 
+
+	const routes = {
+		'/': Dashboard,
+		'/my-curriculum': MyCurriculum,
+		'/grades': Grades,
+		'/grade-alerts': GradeAlerts,
+		'/requirements': Requirements
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button><a href="/">Dashboard</a></button>
+	<button><a href="#/my-curriculum">My Curriculum</a></button>
+	<button><a href="#/grades">Grades</a></button>
+	<button><a href="#/grade-alerts">Grade Alerts</a></button>
+	<button><a href="#/requirements">Requirements</a></button>
+	<Router {routes} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
---
+++ acadmate/src/pages/Requirements.svelte
@@ -0,0 +1,34 @@
+<script>
+    
+</script>
+
+<main>
+    <div class="topnav">
+		<!-- Substitute links below with image elements. -->
+		<a href="/">
+			<img src="/logo_with_title.png" alt="logo" class="logo">
+		</a>
+		<a href="/" style="float: right">Settings</a>
+		<a href="/" style="float: right">Profile</a>
+	</div>
+	
+	<div class="sidenav">
+		<ul>
+		  {#each links as link}
+			<li>
+			  <a href={link.href}
+				 class:active={$page.route.id === link.id}
+				 class:inactive={$page.route.id !== link.id}>
+				<img src={link.icon} alt={link.text} class="icon">
+				{link.text}
+			  </a>
+			</li>
+		  {/each}
+		</ul>
+	  </div>
+	<slot></slot>
+</main>
+
+<style>
+
+</style>
