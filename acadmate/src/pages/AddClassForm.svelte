1f9efa69766e8ba8b02dbfcdb5e0974e7995bdeb
sumulongnj <njsumulong@up.edu.ph>
2023-04-18 05:30:32 +0800

    add and delete class

---
+++ acadmate/src/pages/AddClassForm.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    export let newid;
+    let dispatch = createEventDispatcher();
+    let name;
+    let classGWA;
+    const handleSubmit = () => {
+        const Class = {
+            id: newid,
+            name,
+            classGWA,
+            assessmentList: [],
+        }
+        dispatch('addClass', Class);
+    };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <div style="color: #ac4949; font-size: 25px;">
+        <h3>New Class</h3>
+    </div>
+    <input type="text" placeholder="Class" bind:value={name}>
+    <input type="text" placeholder="GWA" bind:value={classGWA}>
+    <button>Add</button>
+</form>
+
+<style>
+    
+</style>
---
+++ acadmate/src/pages/DelClassForm.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { createEventDispatcher } from "svelte"; 
+    let dispatch = createEventDispatcher();
+
+    const handleSubmit = (e, classDelID) => {
+        dispatch('deleteClass', '{}', classDelID);
+    };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <div style="color: #ac4949">
+        <h3>Delete Class?</h3>
+    </div>
+    <button>Continue</button>
+</form>
+
+<style>
+</style>
