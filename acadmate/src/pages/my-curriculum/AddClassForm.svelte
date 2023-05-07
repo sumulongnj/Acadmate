94cdf762e50c91f719efb605265504ca9fe889fb
sumulongnj <njsumulong@up.edu.ph>
2023-05-07 10:36:47 +0800

    grade input box to dropdown

--- acadmate/src/pages/my-curriculum/AddClassForm.svelte
+++ acadmate/src/pages/my-curriculum/AddClassForm.svelte
@@ -5,6 +5,8 @@
     let name;
     let finalGrade;
     let units;
+    let gradeOptions = ["1.00", "1.25", "1.50", "1.75", "2.00", "2.25", "2.50", "2.75", "3.00", "4.00", "5.00", "DRP", "INC", "0.00"];
+    
     const handleSubmit = () => {
         const Class = {
             id: newid,
@@ -21,7 +23,14 @@
     <h3 style="color: #ac4949">New Class</h3>
     <input type="text" placeholder="Class" bind:value={name}>
     <input type="text" class="block" placeholder="Units" bind:value={units}>
-    <input type="text" class="block" placeholder="Final Grade" bind:value={finalGrade}>
+    <label>
+        Final Grade:
+        <select bind:value={finalGrade}>
+          {#each gradeOptions as grade}
+            <option value={grade}>{grade}</option>
+          {/each}
+        </select>
+      </label>
     <button>Add</button>
 </form>
 
--- acadmate/src/pages/my-curriculum/EditClassForm.svelte
+++ acadmate/src/pages/my-curriculum/EditClassForm.svelte
@@ -8,7 +8,8 @@
     let units = sub.units;
     let name = sub.name;
     let finalGrade = sub.finalGrade;
-  
+    let gradeOptions = ["1.00", "1.25", "1.50", "1.75", "2.00", "2.25", "2.50", "2.75", "3.00", "4.00", "5.00", "DRP", "INC", "0.00"];
+    
     const handleSubmit = () => {
       const updatedClass = {
         ...sub,
@@ -25,7 +26,14 @@
     <h3 style="color: #ac4949">Edit Class</h3>
     <input type="text" placeholder="Class" bind:value={name}>
     <input type="text" class="block" placeholder="Units" bind:value={units}>
-    <input type="text" class="block" placeholder="Final Grade" bind:value={finalGrade}>
+    <label>
+      Final Grade:
+      <select bind:value={finalGrade}>
+        {#each gradeOptions as grade}
+          <option value={grade}>{grade}</option>
+        {/each}
+      </select>
+    </label>
     <button>Edit</button>
 </form>
 
