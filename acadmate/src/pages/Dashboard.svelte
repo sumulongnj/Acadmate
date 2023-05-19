1a7a668470f1362f3ccff9b2211b8c6dd7762329
sumulongnj <njsumulong@up.edu.ph>
2023-05-19 23:03:23 +0800

    dashboard first draft

--- acadmate/src/pages/Dashboard.svelte
+++ acadmate/src/pages/Dashboard.svelte
@@ -1,13 +1,58 @@
 <script>
-    
-</script>
+    import Pie from './Pie.svelte';
 
+    let totalOverallUnits = parseFloat(localStorage.getItem("TotalOverallUnits"));
+    let percent = (totalOverallUnits / 148) * 100;
+</script>
 <main>
-    <div class="Title">
-        <h2>Welcome to Dashboard!</h2>
+    <div class="chart-container">
+        <Pie size={500} percent={percent} />
+        <div class="legend">
+            <div class="legend-item">
+              <div class="legend-square" style="background-color: #ac4949;"></div>
+              <span>Completed Units - {totalOverallUnits}</span>
+            </div>
+            <div class="legend-item">
+              <div class="legend-square" style="background-color: #e28f60;"></div>
+              <span>Remaining Units - {148 - totalOverallUnits}</span>
+            </div>
+        </div>
+    </div>
+    <div class="text">
+        You are {percent}% complete!
     </div>
 </main>
 
 <style>
+    .text {
+        display: flex;
+        justify-content: center;
+        font-size: 30px;
+    }
+    .chart-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+    .legend {
+        justify-content: right;
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+    }
 
-</style>
+    .legend-item {
+        display: flex;
+        align-items: center;
+        margin-right: 10px;
+        margin-bottom: 5px;
+    }
+
+    .legend-square {
+        width: 25px;
+        height: 25px;
+        margin-left: 100px;
+        margin-right: 5px;
+    }
+  </style>
---
+++ acadmate/src/pages/Pie.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let size = 400;
+    export let totalOverallUnits = parseFloat(localStorage.getItem("TotalOverallUnits"));
+    export let percent = (totalOverallUnits / 148) * 100;
+    export let bgColor = '#e28f60';
+    export let fgColor = '#ac4949';
+    
+    $: viewBox = `0 0 ${size} ${size}`;
+    
+    $: radius = size / 2;
+    $: halfCircumference = Math.PI * radius;
+    $: pieSize = halfCircumference * (percent / 100);
+    $: dashArray = `0 ${halfCircumference - pieSize} ${pieSize}`;
+</script>
+  
+<svg width={size} height={size} {viewBox}>
+    <circle r={radius} cx={radius} cy={radius} fill={bgColor} />
+    <circle
+      r={radius / 2}
+      cx={radius}
+      cy={radius}
+      fill={bgColor}
+      stroke={fgColor}
+      stroke-width={radius}
+      stroke-dasharray={dashArray}
+    />
+</svg>
