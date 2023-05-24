465838b6e45bfe6372e18004faa565cdefad190c
shannentan22 <astan9@up.edu.ph>
2023-05-24 17:56:48 +0800

    Fixed Grade Alerts subtitles

--- acadmate/src/pages/GradeAlerts.svelte
+++ acadmate/src/pages/GradeAlerts.svelte
@@ -14,12 +14,14 @@
     <div class="Title">
         <h2>Grade Alerts</h2>
         <!-- <a href="/">Go to Dashboard</a> -->
-    
+    <div class="alertTitle">  
+        <h3>Underloading/Overloading</h3>
+    </div>
     {#each SemesterList as semester}
         {#if semester["underload"]}
-            <h3>Warning: {semester["year"]} {semester["name"]} is underload!<br></h3>
+            <h3>Warning: {semester["year"]} {semester["name"]} is underloaded!<br></h3>
         {:else if semester["overload"]}
-            <h3>Warning: {semester["year"]} {semester["name"]} is overload!<br></h3>
+            <h3>Warning: {semester["year"]} {semester["name"]} is overloaded!<br></h3>
         {/if}
        
     {/each}
@@ -28,5 +30,14 @@
 
 
 <style>
-
+    .alertTitle {
+        background-color: #fce4e4;
+        display: inline-block;
+		border-radius: 20px;
+    }
+    .alertTitle h3 {
+        margin: 5px 18px;
+        font-weight: normal;
+        color: #b25656;
+    }
 </style>
